e117d45880b2623f91d8c84cc0fffa2c11f6c3a8
Vector-7 <seokbong60@gmail.com>
2022-05-10 05:20:35 +0900

    add: docker-compose.yml 및 서버 환경 구축
    but wait-for-it.sh does not work because of grammer

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,5 @@
 from .base import *
-from dotenv import load_dotenv
 
-
-load_dotenv()
 DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -13,10 +10,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'humanscape',
-        'USER': os.environ.get("ROOT"),
-        'PASSWORD': os.environ.get("MYSQL_LOCAL_PASSWORD"),
-        'HOST': os.environ.get("HOST"),
-        'PORT': os.environ.get("PORT")
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get("MYSQL_USER_PASSWORD"),
+		'HOST': os.environ.get("MYSQL_HOST"),
+        'PORT': os.environ.get("MYSQL_PORT")
     }
 }
