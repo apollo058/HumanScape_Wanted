e2aa5f9420e6d8337eee85f42ffe07293a442a3a
Vector-7 <seokbong60@gmail.com>
2022-05-11 13:11:44 +0900

    fix: serializer 불필요한 부분 제거

--- v1/icreat/serializers.py
+++ v1/icreat/serializers.py
@@ -4,18 +4,9 @@ from v1.icreat.models import Icreat
 class IcreatSerializers(serializers.ModelSerializer):
 
     # Date 포맷은 YYYY-MM-DD HH-MM-SS
-    created = serializers.DateTimeField(required=False, format='%Y-%m-%d %H:%M:%S')
-    modified = serializers.DateTimeField(required=False, format='%Y-%m-%d %H:%M:%S')
+    created_at = serializers.DateTimeField(required=False, format='%Y-%m-%d %H:%M:%S')
+    modified_at = serializers.DateTimeField(required=False, format='%Y-%m-%d %H:%M:%S')
 
     class Meta:
         fields = '__all__'
-        read_only_fields = ('created', 'modified', 'id')
         model = Icreat
-
-    def validate(self, data):
-        # timestamp 맘대로 지정/수정 불가
-        NO_MODIFIED = {'created', 'modified'}
-        for k in NO_MODIFIED:
-            if k in data:
-                raise serializers.ValidationError(f"No {k} modified")
-        return data
