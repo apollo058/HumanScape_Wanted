31042a6dfa7bf47b262b215c6f4553c8cfa5d70d
Vector-7 <seokbong60@gmail.com>
2022-05-11 19:34:54 +0900

    fix: requirements.txt 수정

--- run_for_deploy.sh
+++ run_for_deploy.sh
@@ -2,4 +2,5 @@
 python manage.py migrate --settings=config.settings.prod
 python manage.py makemigrations --settings=config.settings.prod
 python manage.py migrate --settings=config.settings.prod
+python manage.py crontab add --settings=config.settings.prod
 python manage.py runserver --settings=config.settings.prod 0.0.0.0:8080
